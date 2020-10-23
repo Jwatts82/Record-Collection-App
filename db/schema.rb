25f02b601b027effe26628c3811b793668cfba48
jwatts82 <jessperr182@hotmail.com>
2020-10-23 00:57:17 -0700

    table update with password

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20201023021051) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
