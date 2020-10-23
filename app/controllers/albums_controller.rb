2c8d6c2d76667ef076ba7da12273b4649aeecba5
jwatts82 <jessperr182@hotmail.com>
2020-10-23 00:16:18 -0700

    edit view set

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -10,19 +10,29 @@ class AlbumsController < ApplicationController
     end
 
     post '/albums' do
-     #   binding.pry
-      #  user = User.find_by(id: params[:user_id])
-       # album = user.albums.build(params)
-        #if album.save
-         #   redirect "/albums/#{album.id}"
-       # else
-        #    redirect '/albums/new'
-        #end
+        user = User.find_by(id: params[:user_id])
+        album = user.albums.build(params)
+        if album.save
+            redirect "/albums/#{album.id}"
+        else
+            redirect '/albums/new'
+        end
     end
+
+    get '/albums/:id/edit' do
+        @users = User.all
+        @post = Post.find_by(params)
+        erb :'posts/edit'
+    end
+
+    patch '/albums/:id' do
+        binding.pry
+    end
+
 
     get '/albums/:id' do
         @album = Album.find_by(id: params[:id])
-
+    
         if @album
             erb :'albums/show'
         else
