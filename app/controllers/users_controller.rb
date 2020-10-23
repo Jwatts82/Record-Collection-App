c6c610d78726ad4548b17813c829c52252371244
jwatts82 <jessperr182@hotmail.com>
2020-10-23 14:19:36 -0700

    add sign up view and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,23 @@
 class UsersController < ApplicationController
-    
-    get '/signup' do
-        redirect_if_logged_in(session)
+
+    get "/signup" do
         erb :'users/signup'
-      end
-    
-    
-      post '/signup' do
-        user = User.where(email: params[:user][:email])
-    
-        if !user.empty?
-          flash[:message] = "This email is already taken. Please pick a new one or log in."
-          redirect '/signup'
-        
-        elsif empty_fields?(params[:user])
-          flash[:message] = "Please fill out the form."
-          redirect '/signup'
-        else
-          user = User.new(params[:user])
-    
-          if user.save
+    end
+
+    post "/users" do
+        # binding.pry
+        user = User.new(
+            name: params[:name],  
+            email: params[:email],  
+            password: params[:password]  
+        )
+        if user.save
             session[:user_id] = user.id
             redirect '/albums'
-          else
-            flash[:message] = "Whoops! Something went wrong. Please try again!"
+        else
             redirect '/signup'
-          end
         end
-      end
-    
+        
+    end
 
 end
