bf7035bcfb07049be942355f1c96f826dfa22f36
jwatts82 <jessperr182@hotmail.com>
2020-10-23 07:07:23 -0700

    add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,19 @@ class SessionsController < ApplicationRecord::Base
         erb :'sessions/login'
     end
 
+    post '/login' do
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect '/albums'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/login'
+    end
+
 end
